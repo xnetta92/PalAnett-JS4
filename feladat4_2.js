c3592ef8133152fe68cd5584da4bed609ba3b60b
xnetta92 <palanett92@gmail.com>
2023-09-29 21:07:10 +0200

    semmi

--- feladat4_2.js
+++ feladat4_2.js
@@ -1,18 +1,24 @@
 const lista = [];
 
-function ValidateEmail(mail) 
-{
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(regisztracios_urlap.email.value))
-  {
-    return (true)
-  }
+function ValidateEmail(email) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(regisztracios_urlap.email.value)) {
+        return (true)
+    }
     alert("Helytelen email cím!")
     return (false)
 }
 
-function compare(){
-    if(document.getElementById('pass').value ===
-            document.getElementById('confirm_password').value) {
+function ValidatePassword(pass) {
+    if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,}$/.test(regisztracios_urlap.pass.value)) {
+        return (true)
+    }
+    alert("Helytelen Jelszó!")
+    return (false)
+}
+
+function compare() {
+    if (document.getElementById('pass').value ===
+        document.getElementById('confirm_password').value) {
         document.getElementById('message').innerHTML = "Egyezik";
     } else {
         document.getElementById('message').innerHTML = "Nem egyezik";
@@ -23,42 +29,44 @@ function compare(){
 document.addEventListener("DOMContentLoaded", () => {
     const urlap = document.forms["regisztracios_urlap"];
     urlap.addEventListener("submit", (event) => {
-        
+
         ValidateEmail(document.regisztracios_urlap.email)
+        ValidatePassword(document.regisztracios_urlap.pass)
         event.preventDefault();
+
 
         const fnev = urlap["fnev"].value;
         const email = urlap["email"].value;
         const pass = urlap["pass"].value;
         const confirm_password = urlap["confirm_password"].value;
         const birthdate = urlap["birthdate"].value;
-        });
-
-        let mindent_kitoltott = true;
-        if (fnev.lenght == 0) {
-            window.alert("A név megadása kötelező");
-            mindent_kitoltott = false;
-        }
-
-        if (email.lenght == 0) {
-            window.alert("Az email megadása kötelező");
-            mindent_kitoltott = false;
-        }
-
-        if(pass.length == 0){
-            window.alert("A jelszó megadása kötelező");
-            mindent_kitoltott = false;
-        }
-
-        if (mindent_kitoltott) {
-            const adatok = {
-                fnev: fnev,
-                email: email,
-                pass: pass,
-                birthdate: birthdate
-        
-            };
-            lista.push(adatok);
-            console.log(lista);
-        }
     });
+
+    let mindent_kitoltott = true;
+    if (fnev.lenght == 0) {
+        window.alert("A név megadása kötelező");
+        mindent_kitoltott = false;
+    }
+
+    if (email.lenght == 0) {
+        window.alert("Az email megadása kötelező");
+        mindent_kitoltott = false;
+    }
+
+    if (pass.length == 0) {
+        window.alert("A jelszó megadása kötelező");
+        mindent_kitoltott = false;
+    }
+
+    if (mindent_kitoltott) {
+        const adatok = {
+            fnev: fnev,
+            email: email,
+            pass: pass,
+            birthdate: birthdate
+        };
+
+        lista.push(adatok);
+        console.log(lista);
+    }
+});
